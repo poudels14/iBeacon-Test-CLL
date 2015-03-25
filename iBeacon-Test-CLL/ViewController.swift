6ea4302cc7833fbe31dda21599a0ed502ba7622e
Sagar Poudel <poudels14@gmail.com>
2015-03-24 23:29:05 -0400

    Initial Commit

---
+++ iBeacon-Test-CLL/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  iBeacon-Test-CLL
+//
+//  Created by Sagar Poudel on 3/24/15.
+//  Copyright (c) 2015 Sagar Poudel. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
